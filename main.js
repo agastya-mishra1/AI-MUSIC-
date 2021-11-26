2603a86fb306743915467d75ce117dedcda2a490
agastya-mishra1 <78002464+agastya-mishra1@users.noreply.github.com>
2021-11-25 20:43:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+song1="";
+song2="";
+
+function preload(){
+    song1=loadSound("music1.mp3");
+    song2=loadSound("music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotResults);
+}
+function modelLoaded(){
+    console.log("PoseNet is Intitalized");
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function gotResults(results){
+    if(results.length >0){
+        console.log(results);
+        scoreRW=results[0].pose.keypoints[10].score;
+        scoreLW=results[0].pose.keypoints[9].score;
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+"leftWristY = "+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
+    }
+}
